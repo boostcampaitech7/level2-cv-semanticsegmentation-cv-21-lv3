34ef6d481097bd1ab0c89048e453bb293253abd0
MinSeok1204 <codename206@gmail.com>
2024-11-18 01:20:32 +0000

    [fix] Model Hooking Error

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import random
+
 import wandb
-import tqdm
+from tqdm import tqdm
 import numpy as np
 import albumentations as A
 
@@ -30,14 +31,19 @@ def train(model,
           val_loader,
           criterion,
           optimizer,
-          scheduler,
           num_epochs,
           interver,
           save_dir,
           classes,
           model_name,
+          mixed_precision,
+          accumulation_steps,
           config
     ):
+    
+    print(f'Start training..')
+    best_dice = 0.
+    scaler = torch.cuda.amp.GradScaler()
     
     # WandB 초기화
     wandb.init(
@@ -50,33 +56,43 @@ def train(model,
     # 모델 구조 로깅
     wandb.watch(model, criterion, log="all", log_freq=100)
 
-    print(f'Start training..')
-    best_dice = 0.
-    
     for epoch in range(num_epochs):
         model.train()
         epoch_loss = 0.0
         
         # Training loop
-        for step, (images, masks) in tqdm.tqdm(enumerate(data_loader), 
+        for step, (images, masks) in tqdm(enumerate(data_loader), 
                                              total=len(data_loader), 
                                              desc=f'Epoch {epoch+1}/{num_epochs}'):
             images, masks = images.cuda(), masks.cuda()
-            outputs = model(images)
-            if isinstance(outputs, dict) and 'out' in outputs:
-                outputs = outputs['out']
+
+            # Train loss
+            with torch.cuda.amp.autocast(enabled=mixed_precision):
+                outputs = model(images)
+                if isinstance(outputs, dict) and 'out' in outputs:
+                    outputs = outputs['out']
+                loss = criterion(outputs, masks)
+                epoch_loss += loss.item()
+
+            # Backpropagation
+            if mixed_precision:   
+                scaler.scale(loss).backward()               
+            else:
+                loss.backward()
             
-            # Loss 계산
-            loss = criterion(outputs, masks)
-            epoch_loss += loss.item()
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        
+            #Gradient Accumulation
+            if(step + 1) % accumulation_steps == 0 or (step + 1) == len(data_loader):
+                if mixed_precision:
+                    scaler.step(optimizer)
+                    scaler.update()
+                else:
+                    optimizer.step()
+                optimizer.zero_grad()
+
         # 에폭당 평균 train loss 계산
         avg_train_loss = epoch_loss / len(data_loader)
-            
+        print(f"Epoch[{epoch+1}/{num_epochs}] Completed. Avg Loss: {avg_train_loss:.4f}")
+        
         # Validation 단계
         if (epoch + 1) % interver == 0:
             # utils.validation에서 구현된 validation 함수 사용
@@ -94,15 +110,18 @@ def train(model,
             if best_dice < dice:
                 print(f"Best performance at epoch: {epoch + 1}, {best_dice:.4f} -> {dice:.4f}")
                 best_dice = dice
+                
+                # model unhooking
+                wandb.unwatch(model)
                 save_model(model, save_dir, model_name)
                 
-                # Best 모델을 WandB에 저장
+                #Best 모델을 WandB에 저장 -> Test 모델 형태랑 충돌
                 artifact = wandb.Artifact(
                     name=f"{model_name}_best", 
                     type="model",
                     description=f"Best model with dice score: {dice:.4f}"
                 )
-                artifact.add_file(f"{save_dir}/{model_name}.pth")
+                artifact.add_file(f"{save_dir}/{model_name}_best_model.pt")
                 wandb.log_artifact(artifact)
 
 def main():
@@ -112,8 +131,9 @@ def main():
     tf = A.Resize(512, 512)
     
     config = load_config('./config/config.json')  # config.json 로드
-    img_root = config['IMAGE_ROOT']
-    label_root = config['LABEL_ROOT']
+    data_root = config['DATA_ROOT']
+    img_root = f"{data_root}/train/DCM"
+    label_root = f"{data_root}/train/outputs_json"
     classes = config['CLASSES']
     batch_size = config['BATCH_SIZE']
     n_class = len(classes)
@@ -168,6 +188,18 @@ def main():
         scheduler_selector = LRSchedulerSelector(optimizer, config['lr_scheduler'])
         scheduler = scheduler_selector.get_scheduler()
 
+    # mixed_precision 설정
+    if 'mixed_precision' in config:
+        mixed_precision = config['mixed_precision']
+    else:
+        mixed_precision = True
+
+    # Gradient Accumulation_steps 설정
+    if 'accumulation_steps' in config:
+        accumulation_steps = config['accumulation_steps']
+    else:
+        accumulation_steps = 1
+
     # wandb에 기록할 config 설정
     wandb_config = {
         "learning_rate": config['LR'],
@@ -191,7 +223,9 @@ def main():
         save_dir=config['SAVED_DIR'],
         classes=config['CLASSES'],
         model_name=config['model']['model_name'],
-        config=wandb_config
+        config=wandb_config,
+        mixed_precision=mixed_precision,
+        accumulation_steps=accumulation_steps
     )
 
 if __name__ == '__main__':
