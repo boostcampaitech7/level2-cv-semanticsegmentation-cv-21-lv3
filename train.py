b6315d9711fefebf78d687031ba1477b89c8cce1
Poodlee <ppp6131@yonsei.ac.kr>
2024-11-25 16:16:44 +0900

    현재 상태 저장

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import wandb
 from tqdm import tqdm
 import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 from torch.utils.data import DataLoader
 import torch.optim as optim
@@ -12,20 +13,42 @@ from utils.dataset import XRayDataset
 from utils.scheduler import LRSchedulerSelector
 from utils.loss import LossSelector
 from trainer.trainer import Trainer  # Trainer 클래스 임포트
+import argparse
+
+from lion_pytorch import Lion
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Model configuration from command line.')
+    parser.add_argument('--model_name', type=str, help='Name of the model', default='FPN')
+    parser.add_argument('--encoder_name', type=str, help='Name of the encoder', default='resnet152')
+    parser.add_argument('--optimizer', type=str, help='Optimizer to use', default='adam')
+    parser.add_argument('--resize', type=int, help='Reshape size', default=512)
+    parser.add_argument('--batch_size', type=int, help='Batch size', default=4)
+    return parser.parse_args()
 
 def main():
+    args = parse_arguments()
+
     # WandB 로그인
     wandb.login()
     
-    tf = A.Resize(512, 512)
-    
+    resize = args.resize
+    # 데이터 증강 및 전처리
+    tf = A.Resize(resize, resize)
+    print('##########################')
+    print(f'resize: {resize}')
+    print('##########################')
+
     config = load_config('./config/config.json')  # config.json 로드
     data_root = config['DATA_ROOT']
     img_root = f"{data_root}/train/DCM"
     label_root = f"{data_root}/train/outputs_json"
     classes = config['CLASSES']
-    batch_size = config['BATCH_SIZE']
+    batch_size = args.batch_size
    
+    config['model']['model_name'] = args.model_name if args.model_name else config['model']['model_name']
+    config['model']['encoder_name'] = args.encoder_name if args.encoder_name else config['model']['encoder_name']
+
     train_dataset = XRayDataset(
         is_train=True,
         transforms=tf,
@@ -44,15 +67,15 @@ def main():
         dataset=train_dataset, 
         batch_size=batch_size,
         shuffle=True,
-        num_workers=8,
+        num_workers=3,
         drop_last=True,
     )
     # 주의: validation data는 이미지 크기가 크기 때문에 `num_workers`는 커지면 메모리 에러가 발생할 수 있습니다.
     valid_loader = DataLoader(
         dataset=valid_dataset, 
-        batch_size=4,
+        batch_size=batch_size,
         shuffle=False,
-        num_workers=8,
+        num_workers=3,
         pin_memory=True,
         drop_last=False
     )
@@ -65,17 +88,28 @@ def main():
     model = model_selector.get_model()
     model = model.cuda()
     
-    # Loss function
-    criterion = nn.BCEWithLogitsLoss()
-    # loss_selector = LossSelector(config['loss'])
-    # criterion = loss_selector.get_loss()
 
+    # Loss function
+    # criterion = nn.BCEWithLogitsLoss()
+    from segmentation_models_pytorch.losses import *
+    # loss_selector = LossSelector(config['loss'])
+    criterion = DiceLoss(mode="multilabel")
+    print(f'criterion is: {criterion}')
     # Optimizer
-    optimizer = optim.Adam(params=model.parameters(), lr=config['LR'], weight_decay=1e-6)
-    
+    if args.optimizer.lower() == 'lion':
+        optimizer = Lion(params=model.parameters(), lr=config['LR'], weight_decay=1e-6)
+    elif args.optimizer.lower() == 'adamw':
+        optimizer = optim.AdamW(params=model.parameters(), lr=config['LR'], weight_decay=1e-6)
+    elif args.optimizer.lower() == 'sgd':
+        optimizer = optim.SGD(params=model.parameters(), lr=config['LR'], momentum=0.9, weight_decay=1e-6)
+    else:
+        optimizer = optim.Adam(params=model.parameters(), lr=config['LR'], weight_decay=1e-6)
+
+    print(f'optimizer: {optimizer}, model_name: {config["model"]["model_name"]}, encoder_name: {config["model"]["encoder_name"]}')
+
     # Trainer 클래스 인스턴스 생성
     sweep_mode = config.get('sweep_config', {}).get('sweep_mode', False)  # sweep_mode 값 가져오기
-    print(sweep_mode)
+    print(f'sweep_mode: {sweep_mode}')
     trainer = Trainer(
         model=model,
         train_loader=train_loader,
--- utils/dataset.py
+++ utils/dataset.py
@@ -158,8 +158,8 @@ class XRayDataset(Dataset):
             result = self.transforms(**inputs)
             
             image = result["image"]
-            label = result["mask"] if self.is_train else label
-
+            label = result["mask"] if self.is_train else label    
+        
         # to tenser will be done later
         image = image.transpose(2, 0, 1)    # channel first 포맷으로 변경합니다.
         label = label.transpose(2, 0, 1)
@@ -167,4 +167,118 @@ class XRayDataset(Dataset):
         image = torch.from_numpy(image).float()
         label = torch.from_numpy(label).float()
             
+        # self.save_visualization(image, label, image_name)
+        # self.save_image_visualization(image,image_name)
+    
         return image, label
+
+    # def __getitem__(self, item):
+    #     image_name = self.filenames[item]
+    #     image_path = os.path.join(self.img_root, image_name)
+        
+    #     image = cv2.imread(image_path)
+    #     image = image / 255.
+        
+    #     label_name = self.labelnames[item]
+    #     label_path = os.path.join(self.label_root, label_name)
+        
+    #     # label 파일 읽기
+    #     with open(label_path, "r") as f:
+    #         annotations = json.load(f)
+    #     annotations = annotations["annotations"]
+        
+    #     if self.is_train:
+    #         # 모든 annotation의 points를 고려하여 가장 작은 x_min, y_min과 가장 큰 x_max, y_max 찾기
+    #         x_min, y_min = float('inf'), float('inf')
+    #         x_max, y_max = float('-inf'), float('-inf')
+    #         for ann in annotations:
+    #             points = np.array(ann["points"])
+    #             x_min = min(x_min, np.min(points[:, 0]))
+    #             y_min = min(y_min, np.min(points[:, 1]))
+    #             x_max = max(x_max, np.max(points[:, 0]))
+    #             y_max = max(y_max, np.max(points[:, 1]))
+            
+    #         # 정사각형으로 만들기
+    #         center_x = (x_min + x_max) / 2
+    #         center_y = (y_min + y_max) / 2
+    #         side_length = max(x_max - x_min, y_max - y_min)
+    #         bbox = [
+    #             int(center_x - side_length / 2),
+    #             int(center_y - side_length / 2),
+    #             int(center_x + side_length / 2),
+    #             int(center_y + side_length / 2)
+    #         ]
+            
+    #         # Bounding box 확장
+    #         expand = 50
+    #         bbox[0] = max(0, bbox[0] - expand)
+    #         bbox[1] = max(0, bbox[1] - expand)
+    #         bbox[2] = min(image.shape[1], bbox[2] + expand)
+    #         bbox[3] = min(image.shape[0], bbox[3] + expand)
+            
+
+    #         # 이미지와 label crop
+    #         image = image[bbox[1]:bbox[3], bbox[0]:bbox[2], :]
+    #         cropped_label = np.zeros((bbox[3] - bbox[1], bbox[2] - bbox[0], len(self.classes)), dtype=np.uint8)
+    #         for ann in annotations:
+    #             c = ann["label"]
+    #             class_ind = self.CLASS2IND[c]
+    #             points = np.array(ann["points"]) - [bbox[0], bbox[1]]  # 좌표 이동
+    #             class_label = np.zeros(cropped_label.shape[:2], dtype=np.uint8)
+    #             cv2.fillPoly(class_label, [points], 1)
+    #             cropped_label[..., class_ind] = class_label
+    #         label = cropped_label
+    #     else:
+    #         # Validation 단계에서는 원본 이미지를 그대로 사용
+    #         label = np.zeros(image.shape[:2] + (len(self.classes),), dtype=np.uint8)
+    #         for ann in annotations:
+    #             c = ann["label"]
+    #             class_ind = self.CLASS2IND[c]
+    #             points = np.array(ann["points"])
+    #             class_label = np.zeros(image.shape[:2], dtype=np.uint8)
+    #             cv2.fillPoly(class_label, [points], 1)
+    #             label[..., class_ind] = class_label
+        
+    #     if self.transforms is not None:
+    #         inputs = {"image": image, "mask": label} if self.is_train else {"image": image}
+    #         result = self.transforms(**inputs)
+            
+    #         image = result["image"]
+    #         label = result["mask"] if self.is_train else label
+
+    #     # Tensor 변환
+    #     image = image.transpose(2, 0, 1)  # channel first
+    #     label = label.transpose(2, 0, 1)
+
+    #     image = torch.from_numpy(image).float()
+    #     label = torch.from_numpy(label).float()
+        
+    #     # self.save_visualization(image, label, image_name)
+
+    #     return image, label
+    
+    def save_visualization(self, image, label, image_name):
+        # 이미지와 label을 시각화하여 저장
+        image_np = image.numpy().transpose(1, 2, 0) * 255  # channel last로 변환
+        image_np = image_np.astype(np.uint8)
+        
+        # 각 클래스에 대해 다른 색상으로 label을 시각화
+        for i in range(label.shape[0]):
+            mask = label[i].numpy()
+            color = (np.random.randint(0, 255), np.random.randint(0, 255), np.random.randint(0, 255))
+            image_np[mask > 0] = image_np[mask > 0] * 0.5 + np.array(color) * 0.5
+
+        # 결과 이미지 저장
+        save_path = os.path.join("visualizations_calcul", f"visual_{image_name}")
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        cv2.imwrite(save_path, image_np)
+        
+    def save_image_visualization(self, image, image_name):
+        # 이미지 시각화하여 저장
+        image_np = image.numpy().transpose(1, 2, 0) * 255  # channel last로 변환
+        image_np = image_np.astype(np.uint8)
+
+        # 결과 이미지 저장
+        save_path = os.path.join("visualizations_image", f"visual_{image_name}")
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        cv2.imwrite(save_path, image_np)
