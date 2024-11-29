9d387d89e8233c527ac07bdfb653b201c411dee6
MinSeok1204 <codename206@gmail.com>
2024-11-29 05:17:13 +0000

    [Fix] remove mmseg

--- mmsegmentation/mmseg/evaluation/metrics/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .citys_metric import CityscapesMetric
-from .depth_metric import DepthMetric
-from .iou_metric import IoUMetric
-from .dice_xraybone import DiceMetric
-
-__all__ = ['IoUMetric', 'CityscapesMetric', 'DepthMetric', 'DiceMetric']
